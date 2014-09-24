7c6a8cc9afa970fd01a7205a00d7e85068103a43
Magnus von Koeller <hello@magnusvk.com>
2014-09-23 20:28:27 -0400

    Associate new exchange order with same store as original order
    When an expedited exchange is not returned, we create a new order so that
    we can charge the customer for the unreturned item. That order doesn't
    currently have a store associated to it, wreaking havoc in our process.
    This makes sure that it will.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,4 +5,14 @@ Spree::Order.class_eval do
   def available_payment_methods
     @available_payment_methods ||= Spree::PaymentMethod.available(:front_end, store)
   end
+
+  def create_order_for_unreturned_exchange_with_store!
+    order = self.create_order_for_unreturned_exchange_without_store!
+
+    # grab the store ID from the original order
+    order.update_attribute(:store_id, self.store_id)
+
+    order
+  end
+  alias_method_chain :create_order_for_unreturned_exchange!, :store
 end
--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -4,15 +4,20 @@ describe Spree::Order do
 
   before(:each) do
     @store = FactoryGirl.create(:store)
-    @order = FactoryGirl.create(:order, :store => @store)
-
-    @order2 = FactoryGirl.create(:order)
   end
 
   it 'should correctly find products by store' do
+    @order = FactoryGirl.create(:order, :store => @store)
+    @order2 = FactoryGirl.create(:order)
+
     by_store = Spree::Order.by_store(@store)
 
     by_store.should include(@order)
     by_store.should_not include(@order2)
+  end
+
+  it 'should associate an unreturned exchange order with the store of the original order' do
+    order = FactoryGirl.create(:shipped_order, line_items_count: 1, store: @store)
+    order.create_order_for_unreturned_exchange!.store_id.should == order.store_id
   end
 end
